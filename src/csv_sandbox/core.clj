4c8d8b8739b53404f46725f83d685769d9d88c8f
nabacg <nabacg@gmail.com>
2014-09-10 17:08:06 +0100

    simplifying with zipmap

--- src/csv_sandbox/core.clj
+++ src/csv_sandbox/core.clj
@@ -4,24 +4,18 @@
            [clojure.java.io :as io]))
 
 
-
-
-
 (defn file->list [path]
   (with-open [input-file (io/reader path)]
     (doall
      (csv/read-csv input-file))))
 
 (defn list->dicts [[header & body]]
-  (map (fn [line]
-         (reduce
-          (fn [val [k v]] (assoc val (keyword k) v))
-          {}
-          (map (fn [k v] [k v]) header line)))
-       body))
+  (let [keys (map keyword header)]
+    (map #(zipmap keys %)
+         body)))
 
 (defn file->dicts [path]
-  (list->dicts (file->dicts path)))
+  (list->dicts (file->list path)))
 
 (defn -main
   "I don't do a whole lot ... yet."
